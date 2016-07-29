9ba5dc6fe72b618db69ed0993b5583246e6906a9
yanjm <2181003078@qq.com>
2016-07-29 15:23:18 +0800

    V2.0 (1)修改默认主页 (2)支持云函数的定义、调用

--- app.js
+++ app.js
@@ -26,8 +26,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+//app.get('/', function(req, res) {
+//  res.render('index', { currentTime: new Date() });
+//});
+
 app.get('/', function(req, res) {
-  res.render('index', { currentTime: new Date() });
+  res.render('img', { currentTime: new Date() });
 });
 
 // 可以将一类的路由单独保存在一个文件中
--- cloud.js
+++ cloud.js
@@ -7,4 +7,11 @@ AV.Cloud.define('hello', function(request, response) {
   response.success('Hello world!');
 });
 
+AV.Cloud.define('god', function(request, response) {
+  response.success('Thanks god!');
+});
+
+AV.Cloud.define('cloudgod', function(request, response) {
+  response.success('Thanks CLOUD=====god!');
+});
 module.exports = AV.Cloud;
